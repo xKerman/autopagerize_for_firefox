5941f356c5f25e6ccf71e92000d88a602d1222e0
X Kerman <xKhorasan@gmail.com>
2011-11-23 17:10:34 +0900

    changed headChildNames according to html5 spec

--- data/autopagerize.user.js
+++ data/autopagerize.user.js
@@ -792,7 +792,8 @@ function createHTMLDocumentByString(str) {
         link: true,
         script: true,
         style: true,
-        base: true
+        base: true,
+        command: true
     }
     var child
     while ((child = fragment.firstChild)) {
